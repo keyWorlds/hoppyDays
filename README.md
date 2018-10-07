a72d71afc054c3223a81b504a60278926e2efb52
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2018-10-07 14:05:06 +0200

    add readme, tileset, collisions and fix player jumping

---
+++ README.md
@@ -0,0 +1,7 @@
+# hoppyDays
+
+Made with [GodotEngine](https://godotengine.org/).
+
+Following [Gamedev TV tutorials](https://www.gamedev.tv/) and using [Kenney.nl assets](https://kenney.nl)
+
+**keyWorlds**
---
+++ project/Scripts/Player.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+var horizontalSpeed = 750
+var grav = 3600
+var verticalSpeed = -1600
+
+var movement = Vector2()
+var UP = Vector2(0, -1)
+
+func _physics_process(delta):
+	update_movement(delta)
+
+func _process(delta):
+	update_animation(movement)
+
+func update_movement(delta):
+	run()
+	fall(delta)
+	jump()
+	move_and_slide(movement, UP)
+
+func update_animation(movement):
+	$AnimatedSprite.update(movement)
+
+func run():
+	movement.x = 0
+	if Input.is_action_pressed("ui_left"):
+		movement.x -= horizontalSpeed
+	if Input.is_action_pressed("ui_right"):
+		movement.x += horizontalSpeed
+		
+
+func jump():
+	if Input.is_action_pressed("ui_up") && is_on_floor():
+		movement.y += verticalSpeed
+
+func fall(delta):
+	if is_on_floor():
+		movement.y = 0
+	elif is_on_ceiling():
+		movement.y = 0
+	else:
+		movement.y += grav * delta
