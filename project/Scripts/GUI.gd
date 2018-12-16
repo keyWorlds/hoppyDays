66140ab5d3a25db1d2e723517f2808e884efb76b
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2018-12-16 11:49:54 +0100

    update GUI when damage!

--- project/Scripts/GUI.gd
+++ project/Scripts/GUI.gd
@@ -2,3 +2,6 @@ extends CanvasLayer
 
 func _ready():
 	Global.GUI = self
+
+func updateGUI(life_points):
+	$Banner/HBoxContainer/LifeCounter.text = str(life_points)
--- project/Scripts/GameState.gd
+++ project/Scripts/GameState.gd
@@ -7,12 +7,17 @@ func _ready():
 	Global.GameState = self
 	print(Global.GameState)
 	life_points = initial_life_points
+	updateGUI()
 
 func ouch():
 	life_points -= 1
 	Global.Player.damage()
+	updateGUI()
 	if life_points < 0:
 		the_end()
+
+func updateGUI():
+	Global.GUI.updateGUI(life_points)
 
 func the_end():
 	get_tree().change_scene("res://scenes/GameOver.tscn")
