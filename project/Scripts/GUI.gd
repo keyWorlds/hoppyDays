a9e8be404cfa986ed57dc3ab70b6effeb9708a29
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2019-01-07 14:10:42 +0100

    improve GUI (animations)

--- project/Scripts/GUI.gd
+++ project/Scripts/GUI.gd
@@ -3,5 +3,9 @@ extends CanvasLayer
 func _ready():
 	Global.GUI = self
 
-func updateGUI(life_points):
+func updateGUI(coins, life_points):
+	$Banner/HBoxContainer/CoinCounter.text = str(coins)
 	$Banner/HBoxContainer/LifeCounter.text = str(life_points)
+
+func animate(animation):
+	$AnimationPlayer.play(animation)
--- project/Scripts/GameState.gd
+++ project/Scripts/GameState.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 export var initial_life_points = 5
+export var coin_target = 3
+
 var life_points
 var coins = 0
+
+onready var GUI = Global.GUI
 
 func _ready():
 	Global.GameState = self
@@ -13,15 +17,30 @@ func _ready():
 func ouch():
 	life_points -= 1
 	Global.Player.damage()
+	animateGUI("Hurt")
 	updateGUI()
 	if life_points < 0:
 		the_end()
 
 func updateGUI():
-	Global.GUI.updateGUI(life_points)
+	GUI.updateGUI(coins, life_points)
 
 func coinUp():
 	coins += 1
+	animateGUI("CoinPulse")
+	updateGUI()
+	
+	var multiple_of_coin_target = (coins % coin_target) == 0
+	if multiple_of_coin_target:
+		lifeUp()
+
+func lifeUp():
+	life_points += 1
+	animateGUI("LifePulse")
+	updateGUI()
+
+func animateGUI(animation):
+	GUI.animate(animation)
 
 func the_end():
 	get_tree().change_scene("res://scenes/GameOver.tscn")
