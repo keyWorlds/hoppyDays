cb7db40260cfd7c431e3b001ca6ea8af6e3dd15d
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2019-01-07 14:14:44 +0100

    fix bug (infinite coin adding)

--- project/Scripts/Coin.gd
+++ project/Scripts/Coin.gd
@@ -1,8 +1,12 @@
 extends AnimatedSprite
 
+var taken = false
+
 func _on_Area2D_body_entered(body):
-	Global.GameState.coinUp()
-	$AnimationPlayer.play("pickedup");
+	if not taken:
+		taken = true
+		Global.GameState.coinUp()
+		$AnimationPlayer.play("pickedup");
 
 func destroy():
 	queue_free()
